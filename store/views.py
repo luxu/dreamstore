be2ddf26b3d03ffb19d31e4661490da4a9ccad56
Luciano Martins <zicadopv@gmail.com>
2024-11-18 06:39:17 -0300

    Update in the file views.py, pyproject.toml and requirements.txt

--- store/views.py
+++ store/views.py
@@ -1,7 +1,6 @@
 import json
 
 from django.http import JsonResponse, HttpResponse, HttpRequest
-from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Product
@@ -16,7 +15,13 @@ def webhook(request: HttpRequest) -> HttpResponse:
         response_text = opcoes_intent(req)
 
         return JsonResponse({
-            "fulfillmentText": response_text
+            "fulfillmentText": response_text,
+            "outputContexts": [
+                {
+                    "name": f"{req.get('session')}/contexts/awaiting_product_choice",
+                    "lifespanCount": 5
+                }
+            ]
         })
     else:
         return HttpResponse()
@@ -24,93 +29,83 @@ def webhook(request: HttpRequest) -> HttpResponse:
 
 def opcoes_intent(req) -> str:
     intent_name = req.get('queryResult').get('intent').get('displayName')
+    print(intent_name)
 
     if intent_name == 'Default Welcome Intent':
         response_text = """Olá! Seja bem-vindo(a)! Eu sou o Ravis(Assistente virtual da DreamStore)
-                Deseja iniciar o atendimento?
-                1️⃣Sim
-                2️⃣Não"""
-    elif intent_name == 'resposta-sim':
-        response_text = """Para agilizar o atendimento. Por favor, escolha uma das opções abaixo:
-                1️⃣Lista de produtos
-                2️⃣Itens comprados
-                3️⃣Falar com atendente
-                4️⃣Encerrar Atendimento
-            """
-    elif intent_name == 'respostaNao':
-        response_text = """Ok. Precisando, estamos a disposição para melhor atendê-lo!
-        Querendo retornar ao menu de opções digite: MENU. Bye, Bye!"""
-    elif intent_name == 'ListarProdutos':
+               Deseja iniciar o atendimento?
+               - SIM
+               - NÃO"""
+    # elif intent_name == 'respostaNao':
+    #     response_text = """Ok. Precisando, estamos a disposição para melhor atendê-lo!Bye, Bye!
+    # """
+    elif intent_name == 'answerNo':
+        response_text = """Pôxa que pena...
+        
+        Siga-nos nas nossas redes sociais:
+        
+        @instagram
+        @facebook
+        @X (antigo Twitter)
+        
+        Para finalizar, digite SAIR!        
+        """
+    elif intent_name == 'answerYes':
+        total_length = 30
+        fill_char = "="
+        name = req.get('queryResult').get('parameters').get('name')
         products = Product.objects.all()
-        lista_products = [f"{product.name} - R$ {product.price}- Quant.: {product.stock}" for product in products]
-        response_text = "Aqui está a lista de produtos:\n" + "\n".join(lista_products)
-    elif intent_name == 'BuscarProduto':
-        parameters = req.get('queryResult').get('parameters')
-        name = parameters.get('name')
-        try:
-            product = Product.objects.get(name__icontains=name)
-            response_text = f"ID..: {product.id} - {product.name} - R$ {product.price}- Quant.: {product.stock}"
-        except Product.DoesNotExist:
-            response_text = "Desculpe, produto não existe em nossa loja!"
-        return response_text
-    elif intent_name == 'CadastrarProduto':
-        parameters = req.get('queryResult').get('parameters')
-        name = parameters.get('name')
-        price = parameters.get('price')
-        stock = parameters.get('stock')
-        payload = {
-            'name': name,
-            'price': price,
-            'stock': stock
-        }
-        product = Product.objects.create(**payload)
-        response_text = f"Produto: {product.name} - R$ {product.price} - Quantidade: {product.stock}"
-    elif intent_name == 'AtualizarProduto':
-        parameters = req.get('queryResult').get('parameters')
-        old_name = parameters.get('old_name')[0]
-        new_name = parameters.get('new_name')[0]
-        try:
-            product = Product.objects.get(name__icontains=old_name[0])
-            product.price
-            product.stock
-            payload = {
-                'name': new_name,
-                'price': price,
-                'stock': stock
-            }
-            p = Product.objects.update_or_create(**payload)
-            response_text = (f"ID..: {p.id} - {p.name} - R$ {p.price}- Quant.: {p.stock}"
-                             f" atualizado com sucesso!")
-        except Product.DoesNotExist:
-            response_text = "Desculpe, produto não existe em nossa loja!"
-        return response_text
+        first_string = f"""Ok. {name}. 
+        Escolha o produto:
+        """
+        response = first_string
+        for product in products:
+            response += f"""
+            {product.name.center(total_length, fill_char).upper()}
+            """
+        response_text = response
+    elif intent_name == 'productChoice':
+        product = req.get('queryResult').get('parameters').get('product')
+        response_text = f'Voce escolheu o {product}. Posso confirmar?'
+    elif intent_name == 'productCancel':
+        response_text = '''Que pena.
+        Para voltar ao menu Principal. Digite VOLTAR.
+        Para sair do atendimento. Digite SAIR.
+        '''
+    elif intent_name == 'productConfirmYes':
+        name = req['queryResult']['outputContexts'][0].get('parameters')['name']
+        product = req['queryResult']['outputContexts'][0].get('parameters')['product']
+        response_text = f'''Ótimo sr(a). {name}. Produto {product} adicionado com sucesso!
+        Para voltar ao menu Principal. Digite VOLTAR.
+        Para sair do atendimento. Digite SAIR.
+        '''
+    # elif intent_name == 'BuscarProduto':
+    #     parameters = req.get('queryResult').get('parameters')
+    #     name = parameters.get('name')
+    #     try:
+    #         product = Product.objects.get(name__icontains=name)
+    #         response_text = f"ID..: {product.id} - {product.name} - R$ {product.price}- Quant.: {product.stock}"
+    #     except Product.DoesNotExist:
+    #         response_text = "Desculpe, produto não existe em nossa loja!"
+    # elif intent_name == 'Comprar':
+    #     products = Product.objects.all()
+    #     list_products = []
+    #     for product in products:
+    #         str_product = "-".join((str(product.id), product.name))
+    #         list_products.append(str_product)
+    #     response_text = f"Temos os produtos: {list_products}. Por favor, escolha qual desses quer comprar:"
+    # elif intent_name == "EscolhaDoProduto":
+    #     requisition = req.get('queryResult').get('parameters')
+    #     product_number = requisition.get('product_number')
+    #     p = req.get('queryResult').get('parameters', {}).get('product_number')
+    #     try:
+    #         product = Product.objects.get(id=product_number)
+    #         response_text = f"ID..: {product.id} - {product.name} - R$ {product.price}- Quant.: {product.stock}"
+    #     except Product.DoesNotExist:
+    #         response_text = "Desculpe, produto não existe em nossa loja!"
+    elif intent_name == 'dreamStoreExit':
+        response_text = 'Muito obrigado interagir comigo, até mais!'
     else:
         response_text = "Intenção não reconhecida"
 
     return response_text
-
-
-
-# def dialogflow_webhook(request):
-#     if request.method == 'POST':
-#         req = json.loads(request.body)
-#
-#         # Extraia a intenção e parâmetros
-#         intent_name = req.get('queryResult').get('intent').get('displayName')
-#
-#         # Lógica personalizada para cada intenção
-#         if intent_name == 'SuaIntencaoEspecifica':
-#             response_text = "Resposta personalizada para a intenção"
-#         else:
-#             response_text = "Intenção não reconhecida"
-#
-#         # Responder ao Dialogflow com fulfillmentMessages
-#         return JsonResponse({
-#             "fulfillmentMessages": [
-#                 {
-#                     "text": {
-#                         "text": [response_text]
-#                     }
-#                 }
-#             ]
-#         })
